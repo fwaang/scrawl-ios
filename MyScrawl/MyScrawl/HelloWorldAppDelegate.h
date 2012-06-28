6fefca00f1bf461231aae219a28f1d538a06fa1c
Frank Wang <frank2336@gmail.com>
2012-06-28 20:16:51 +0800

    Add MyScrawl application.

---
+++ MyScrawl/MyScrawl/HelloWorldAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HelloWorldAppDelegate.h
+//  MyScrawl
+//
+//  Created by Wang Frank on 12-6-28.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyScrawl/MyScrawl/HelloWorldViewController.h
@@ -0,0 +1,13 @@
+//
+//  HelloWorldViewController.h
+//  MyScrawl
+//
+//  Created by Wang Frank on 12-6-28.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController
+
+@end
